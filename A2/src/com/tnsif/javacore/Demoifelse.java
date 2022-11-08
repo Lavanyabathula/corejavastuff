a976efa506a21d4f1c9001c16efe151fdbb48a87
Lavanya Bathula <Lavanya Bathula@TNSIF>
2022-11-08 14:44:08 +0530

    java basics

---
+++ A2/src/com/tnsif/javacore/Demoifelse.java
@@ -0,0 +1,56 @@
+package com.tnsif.javacore;
+
+//whether a given number is even or odd
+public class Demoifelse {
+
+	public static void main(String args[]) {
+		
+//		int x=12;
+//		
+//		if(x%2==0) {
+//			
+//			System.out.println("the given number is Even");
+//			
+//		}
+//		else {
+//			System.out.println("the given number is odd");
+//
+//		}
+//		
+		
+		//whether agiven number is postive or negative or zero
+//		int x=4;
+//		
+//		if(x>0) {
+//			System.out.println("The given number is positive");
+//		}
+//		else if(x<0){
+//			System.out.println("The given number is negative");
+//
+//		}
+//		else {
+//			System.out.println("The given number is zero");
+//
+//		}
+		
+	//whether given number is positive and also even	
+		
+	int x=3;
+	
+	if(x>0) {
+		if(x%2==0) {
+			System.out.println("The given number is positive and even");
+		}
+		else {
+			System.out.println("The given number is positive but not even");
+
+		}
+	}
+	else {
+		System.out.println("The given number is negative");
+
+	}	
+			
+	}
+	
+}
---
+++ A2/src/com/tnsif/javacore/Demoloop.java
@@ -0,0 +1,32 @@
+package com.tnsif.javacore;
+
+public class Demoloop {
+
+	public static void main(String[] args) {
+      
+//		//for loop
+//		for(int i=1;i<11;i++) {
+//			System.out.println("5*"+i+"="+5*i);
+//		}
+//		
+//		//while loop
+//		int evennumber=2;
+//		int i=0;
+//		System.out.println("evennumber's up to 10");
+//
+//		while(i<10) {
+//			System.out.println(evennumber);
+//			evennumber=evennumber+2;
+//			i++;
+//		}
+		
+		//do while
+		int i=10;
+		do {
+			System.out.println("The i value"+i);
+			i--;
+		}while(i>1);
+		
+	}
+
+}
